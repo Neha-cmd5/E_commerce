e964d7766b1310dbafc7c56ddae5f208f78290b5
Neha Saha <138052772+Neha-cmd5@users.noreply.github.com>
2025-08-19 11:54:30 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux'
-import store from './store'
-import App from './App';
-import "./App.css"
+import { combineReducers } from "redux";
+import {
+    productsListReducer,
+    productDetailsReducer,
+    createProductReducer,
+    updateProductReducer,
+    deleteProductReducer,
+    changeDeliveryStatusReducer,
+} from "./productReducers";
 
-ReactDOM.render(
-  <Provider store = {store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
+import {
+    createCardReducer,
+    chargeCardReducer,
+    savedCardsListReducer,
+    deleteSavedCardReducer,
+    updateStripeCardtReducer
+} from "./cardReducers";
 
+import {
+    userLoginReducer,
+    userRegisterReducer,
+    userDetailsReducer,
+    userDetailsUpdateReducer,
+    deleteUserAccountReducer,
+    checkTokenValidationReducer,
+    getSingleAddressReducer,
+    getAllAddressesOfUserReducer,
+    createUserAddressReducer,
+    updateUserAddressReducer,
+    deleteUserAddressReducer,
+    getAllOrdersReducer,
+} from "./userReducers";
+
+const allReducers = combineReducers({
+    productsListReducer,
+    productDetailsReducer,
+    createProductReducer,
+    updateProductReducer,
+    deleteProductReducer,
+    createCardReducer,
+    chargeCardReducer,
+    savedCardsListReducer,
+    updateStripeCardtReducer,
+    deleteSavedCardReducer,
+    userLoginReducer,
+    userRegisterReducer,    
+    getSingleAddressReducer,
+    getAllAddressesOfUserReducer,
+    createUserAddressReducer,
+    updateUserAddressReducer,
+    deleteUserAddressReducer,
+    getAllOrdersReducer,
+    changeDeliveryStatusReducer,
+    checkTokenValidationReducer,
+    userDetailsReducer,
+    userDetailsUpdateReducer,
+    deleteUserAccountReducer,
+})
+
+
+export default allReducers
